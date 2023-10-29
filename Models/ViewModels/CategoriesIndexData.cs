ac90d1139977531d37a07493990318480f25ba7f
Giorgia <cocis_giorgia@yahoo.com>
2023-10-29 17:09:58 +0200

    Lab4

---
+++ Models/ViewModels/CategoriesIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Laborator2_MPD.Models.ViewModels
+{
+    public class CategoriesIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
